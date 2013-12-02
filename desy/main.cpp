041c030630c2721ad5bbc2cc8a1047c4af14e361
Laurent Forthomme <laurent.forthomme@uclouvain.be>
2013-12-02 18:36:12 +0100

    further improvements in the c++ caller

--- desy/main.cpp
+++ desy/main.cpp
@@ -21,12 +21,22 @@ main() {
   int two = 2;
   int three = 3;
 
+  int pdgid, parentid, firstdaughterid, lastdaughterid;
+  bool isstable;
+
   zduini_();
   printlhe_(&one);
   for (int i=0; i<1e2; i++) {
     zduevt_(&one);
     for (int p=0; p<pyjets_.n; p++) {
-      cout << "--> " << p << " = " << pyjets_.p[0][p] << "\t" << pyjets_.p[1][p] << "\t" << pyjets_.p[2][p] << "\t" << pyjets_.p[3][p] << endl;
+      pdgid = pyjets_.k[1][p];
+      parentid = pyjets_.k[2][p];
+      firstdaughterid = pyjets_.k[3][p];
+      lastdaughterid = pyjets_.k[4][p];
+      isstable = pyjets_.v[4][p]==0.;
+      cout << "--> " << p << " = " << pyjets_.p[0][p] << "\t" << pyjets_.p[1][p] << "\t" << pyjets_.p[2][p] << "\t" << pyjets_.p[3][p] << "\t" << pyjets_.p[4][p] << endl;
+      cout << "    " << pyjets_.v[0][p] << "\t" << pyjets_.v[1][p] << "\t" << pyjets_.v[2][p] << "\t" << pyjets_.v[3][p] << "\t" << pyjets_.v[4][p] << endl;
+      cout << "    " << pyjets_.k[0][p] << "\t" << pyjets_.k[1][p] << "\t" << pyjets_.k[2][p] << "\t" << pyjets_.k[3][p] << "\t" << pyjets_.k[4][p] << endl;
     }
     printlhe_(&two);
   }
