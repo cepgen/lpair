a04f4253743e11470934f8a241281f5707be3c4c
Laurent Forthomme <laurent.forthomme@uclouvain.be>
2013-12-03 09:13:06 +0100

    introduced ROOT to have TTree capabilities

--- desy/main.cpp
+++ desy/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include "TTree.h"
+#include "TLorentzVector.h"
+
 using namespace std;
 
 extern "C" {
@@ -16,33 +19,73 @@ extern "C" {
   } pyjets_;
 }
 
-main() {
-  int one = 1;
-  int two = 2;
-  int three = 3;
+Int_t main() {
+  Int_t one = 1;
+  /*Int_t two = 2;
+  Int_t three = 3;*/
 
-  int pdgid, parentid, firstdaughterid, lastdaughterid;
-  bool isstable;
+  Int_t parentid, firstdaughterid, lastdaughterid;
+
+  const Int_t maxpart = 100;
+
+  Int_t npart;
+  Double_t eta[maxpart], phi[maxpart], rapidity[maxpart], px[maxpart], py[maxpart], pz[maxpart], pt[maxpart], E[maxpart], M[maxpart];
+  Int_t PID[maxpart], isstable[maxpart];
+
+  TTree *t;
+  TLorentzVector *mom;
+
+  t = new TTree("h4444", "A TTree containing information from the events produced from LPAIR");
+  mom = new TLorentzVector();
+
+  t->Branch("npart", &npart, "npart/I");
+  t->Branch("eta", eta, "eta[npart]/D");
+  t->Branch("phi", phi, "phi[npart]/D");
+  t->Branch("rapidity", rapidity, "rapidity[npart]/D");
+  t->Branch("px", px, "px[npart]/D");
+  t->Branch("py", py, "py[npart]/D");
+  t->Branch("pz", pz, "pz[npart]/D");
+  t->Branch("pt", pt, "pt[npart]/D");
+  t->Branch("PID", PID, "PID[npart]/I");
+  t->Branch("stable", isstable, "isstable[npart]/I");
+  t->Branch("E", E, "E[npart]/D");
+  t->Branch("M", M, "M[npart]/D");
 
   zduini_();
-  printlhe_(&one);
-  for (int i=0; i<1e2; i++) {
+  //printlhe_(&one);
+  for (Int_t i=0; i<1e2; i++) {
     zduevt_(&one);
-    for (int p=0; p<pyjets_.n; p++) {
-      pdgid = pyjets_.k[1][p];
+    npart = pyjets_.n;
+    for (Int_t p=0; p<pyjets_.n; p++) {
+      PID[p] = pyjets_.k[1][p];
       parentid = pyjets_.k[2][p];
       firstdaughterid = pyjets_.k[3][p];
       lastdaughterid = pyjets_.k[4][p];
-      isstable = pyjets_.v[4][p]==0.;
+      isstable[p] = pyjets_.v[4][p]==0.;
+      px[p] = pyjets_.p[0][p];
+      py[p] = pyjets_.p[1][p];
+      pz[p] = pyjets_.p[2][p];
+      E[p] = pyjets_.p[3][p];
+      M[p] = pyjets_.p[4][p];
+      pt[p] = sqrt(pow(px[p],2)+pow(py[p],2));
+      
       cout << "--> " << p << " = " << pyjets_.p[0][p] << "\t" << pyjets_.p[1][p] << "\t" << pyjets_.p[2][p] << "\t" << pyjets_.p[3][p] << "\t" << pyjets_.p[4][p] << endl;
-      cout << "    " << pyjets_.v[0][p] << "\t" << pyjets_.v[1][p] << "\t" << pyjets_.v[2][p] << "\t" << pyjets_.v[3][p] << "\t" << pyjets_.v[4][p] << endl;
-      cout << "    " << pyjets_.k[0][p] << "\t" << pyjets_.k[1][p] << "\t" << pyjets_.k[2][p] << "\t" << pyjets_.k[3][p] << "\t" << pyjets_.k[4][p] << endl;
+      mom->SetXYZM(px[p], py[p], pz[p], M[p]);
+      eta[p] = mom->PseudoRapidity();
+      rapidity[p] = mom->Rapidity();
+      phi[p] = mom->Phi();
+      //cout << "    " << pyjets_.v[0][p] << "\t" << pyjets_.v[1][p] << "\t" << pyjets_.v[2][p] << "\t" << pyjets_.v[3][p] << "\t" << pyjets_.v[4][p] << endl;
+      //cout << "    " << pyjets_.k[0][p] << "\t" << pyjets_.k[1][p] << "\t" << pyjets_.k[2][p] << "\t" << pyjets_.k[3][p] << "\t" << pyjets_.k[4][p] << endl;
     }
-    printlhe_(&two);
+    t->Fill();
+    //printlhe_(&two);
   }
-  printlhe_(&three);
-  cout << "-> " << beam_.pmod << endl;
-  cout << "-> " << beam_.ipair << endl;
-  cout << "-> " << beam_.inpe << endl;
-  cout << "-> " << beam_.inpp << endl;
+  //printlhe_(&three);
+
+  t->SaveAs("events.root");
+
+  delete mom;
+  delete t;
+  
+  return 0;
 }
