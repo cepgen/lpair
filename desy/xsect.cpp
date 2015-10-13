6cd91f44b00bba3423806a5849f087416d01acaf
Laurent Forthomme <laurent.forthomme@uclouvain.be>
2015-10-13 22:45:53 +0200

    Wrong common block structure in xsect exec

--- desy/xsect.cpp
+++ desy/xsect.cpp
@@ -27,10 +27,16 @@ extern "C" {
   } vgres_;
 
   extern struct {
-    double cotth1,cotth2,ecut,ptcut,mxmin2,mxmax2,thmax,thmin,qp2min,qp2max;
+    double cotth1,cotth2,ecut,ptcutmin,ptcutmax,mxmin2,mxmax2;
+    float thmax,thmin;
+    double qp2min,qp2max;
     int modcut;
-    double mxmn,mxmx,q2mn,q2mx;
+    float mxmn,mxmx,q2mn,q2mx;
   } cuts_;
+}
+
+double eta_to_theta(double eta) {
+  return 2*atan(exp(eta))/acos(-1.)*180.;
 }
 
 int main() {
@@ -41,28 +47,30 @@ int main() {
   // Beam parameters
   beam_.inpe = beam_.inpp = 3500.;
   beam_.pmod = 11;
-  //beam_.pmod = 2;
+  beam_.pmod = 2;
   beam_.emod = 2;
 
   // Vegas parameters
   vegpar_.nprn = 0;
 
   // Outgoing leptons kinematics
-  cuts_.ecut = 0.;
-  cuts_.thmin = 2*atan(exp(-2.5))/acos(-1.)*180.;
-  cuts_.thmax = 2*atan(exp(2.5))/acos(-1.)*180.;
-  cuts_.mxmx = 1000.;
+  //cuts_.ecut = 0.;
+  cuts_.thmin = eta_to_theta(-5.);
+  cuts_.thmax = eta_to_theta( 5.);
+  //cuts_.mxmx = 1000.;
   cout << "Theta in range [" << cuts_.thmin << ", " << cuts_.thmax << "]" << endl;
 
+  cout << "modcut = " << cuts_.modcut << endl;
+  
   ofstream cs("xsect_scan_lpair_singleinelastic.dat");
   //ofstream cs("xsect_scan_lpair_elastic.dat");
 
-  for (int i=0; i<50; i++) {
-    cuts_.ptcut = 0.+i*0.5;
+  for (int i=0; i<100; i++) {
+    cuts_.ptcutmin = 0.+i*0.5;
     gmubeg_();
 
-    cs << cuts_.ptcut << "\t" << vgres_.s1 << "\t" << vgres_.s2 << endl;
-    std::cout << "Pt > " << cuts_.ptcut << " GeV : xsec = " << vgres_.s1 << ", error = " << vgres_.s2 << std::endl;  
+    cs << cuts_.ptcutmin << "\t" << vgres_.s1 << "\t" << vgres_.s2 << endl;
+    std::cout << "Pt > " << cuts_.ptcutmin << " GeV : xsec = " << vgres_.s1 << ", error = " << vgres_.s2 << std::endl;  
   }
   
   return 0;
